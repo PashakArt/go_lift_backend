7a124bec6af4c8d4a66bda531c86ae91ce3e5375
PashakArt <pashakart961@gmail.com>
2026-08-16 11:41:04 +0300

    fix: fixed get training days

--- services/workout-service/internal/repository/queries/get_training_days.sql
+++ services/workout-service/internal/repository/queries/get_training_days.sql
@@ -1,5 +1,6 @@
 SELECT DISTINCT TO_CHAR(DATE(started_at), 'YYYY-MM-DD') as workout_date
 FROM workout_sessions
+JOIN workout_sets ws ON ws.session_id = workout_sessions.session_id
 WHERE user_id = $1 
   AND is_active = false
   AND started_at >= $2 AND started_at < $3;
